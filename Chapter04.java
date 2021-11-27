55853c401cd9e62068a7fe97664a32b7a8fdc87b
yanagisawa-253 <anaguma253@gmail.com>
2021-11-27 09:41:00 +0900

    java学習基礎

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "柳澤千絵";
+		System.out.println(str);
+		str = "java難しい";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
