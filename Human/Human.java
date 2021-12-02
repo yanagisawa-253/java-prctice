c0e3687a0d55b3b8fa2566e64124767621b5699d
yanagisawa-253 <anaguma253@gmail.com>
2021-12-02 20:23:23 +0900

    java基礎学習

--- Human/Human.java
+++ Human/Human.java
@@ -10,11 +10,15 @@ public class Human {
         this.age = age;
     }
 
-    public String getName() {
+    final public String getName() {
         return this.name;
     }
 
     public int getAge() {
         return this.age;
+    }
+
+    public String getProfile() {
+        return "名前は" + this.name + "、年齢は" + this.age + "です。";
     }
 }
