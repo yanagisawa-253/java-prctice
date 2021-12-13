d32303beefb97800629cb7a99b423f75249f4611
yanagisawa-253 <anaguma253@gmail.com>
2021-12-13 23:17:16 +0900

    定数クラスを読みやすく(内部クラス)

---
+++ inner_class_sample/Constants.java
@@ -0,0 +1,12 @@
+package inner_class_sample;
+
+public class Constants {
+
+	public static final String CAT_NAME = "猫";
+	
+	public static final int CAT_LEG_COUNT = 4;
+	
+	public static final String OCTOPUS_NAME = "たこ";
+	
+	public static final int OCTOPUS_LEG_COUNT = 8;
+}
