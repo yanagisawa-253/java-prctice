f42f5ff7f8fe4a76bb9c6ffaaf752921da655b45
yanagisawa-253 <anaguma253@gmail.com>
2021-12-13 23:03:19 +0900

    内部クラスの使い方

--- inner_class_sample/Example.java
+++ inner_class_sample/Example.java
@@ -4,6 +4,11 @@ public class Example {
 
 	public static void main(String[] ages) {
 		Outer outer = new Outer();
-		Outer.Inner inner = outer.new Inner();
+		Outer.Inner inner = outer.new Inner(); //別のクラスCからインスタンス化
 	}
+	
+	public static void main(String[] args) {
+        Outer outer = new Outer(); // クラスOuterをインスタンス化
+        Outer.Inner inner = new Outer.Inner(); // staticな内部クラスBをインスタンス化
+    }
 }
