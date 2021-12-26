c273f8d19836566d191f839cf0f58aa5bb8292b8
yanagisawa-253 <anaguma253@gmail.com>
2021-12-26 22:25:57 +0900

    複数の組み合わせの定義(列拳型)

--- Noclass/Main02.java
+++ Noclass/Main02.java
@@ -3,7 +3,11 @@ package Noclass;
 public class Main02 {
 
 	public static void main(String[] args) {
-		Fruit flt = Fruit.ORANGE;               //enumを取得
-        System.out.println( flt.toString() );   //ORANGE
+//		Fruit flt = Fruit.ORANGE;               //enumを取得
+//      System.out.println( flt.toString() );   //ORANGE
+		Fruit frt = Fruit.APPLE;
+		System.out.println(frt.getLabel());
+		
+		System.out.println(Fruit.PEACH.getId());
 	}
 }
