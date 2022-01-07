ff7b008ae1ebbe14a7c3bb88824e22ddc12cc810
yanagisawa-253 <anaguma253@gmail.com>
2022-01-07 21:07:51 +0900

    age1.2を２倍すると６０より大きい条件式

--- coding/Main3.java
+++ coding/Main3.java
@@ -8,5 +8,10 @@ public class Main3 {
 	    	System.out.println("等しい");
 	  }
 	  
+	  int age1 = 20;
+	  int age2 = 50;
+	  if ((age1 + age2)*2 == 60) {
+		  System.out.println("６０越え");
+	  }
 	}
 }
