ca00c8e0d97caa60fcde8b07094415f00101dc50
yanagisawa-253 <anaguma253@gmail.com>
2022-01-18 21:39:46 +0900

    継承について再確認

---
+++ coding/Main15.java
@@ -0,0 +1,11 @@
+package coding;
+
+public class Main15 extends MainBase {
+
+	public Main15() {
+		public Main15(String employeeName) {
+			super.setEmployeeName(employeeName);
+			super.setDivisionName();
+		}
+	}
+}
---
+++ coding/MainBase.java
@@ -0,0 +1,11 @@
+package coding;
+
+public class MainBase {
+
+	String employeeName;
+	String divisionName;
+	
+	public MainBase() {
+		
+	}
+}
